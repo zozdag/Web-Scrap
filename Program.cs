0c341d42df0ad94789a06cccd47906b8f4fef4ad
Alper <37253853+zozdag@users.noreply.github.com>
2019-12-20 17:28:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScrapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kaynak = @"https://www.yeditepe.edu.tr/"; // URL YAZDIK
+            HtmlWeb web = new HtmlWeb(); // Birtan Nesne oluşturduk
+            var Rawresult = web.Load(kaynak); // nesnemizin load methoduna ulaşarak parametresine kaynak adlı urimizi verdik
+
+            var Result = Rawresult.DocumentNode.SelectSingleNode("//head/title"); // istediğimiz html tagını belirttik
+
+            Console.WriteLine(Result.OuterHtml); // ekrana basttık.
+            Console.ReadLine();
+        }
+    }
+}
